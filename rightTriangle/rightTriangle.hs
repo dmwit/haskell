e97d42e291aa9ff545befb1e05f0c998308e2c39
mr-fool <mr-fool@users.noreply.github.com>
2015-01-18 22:24:49 -0700

    Update rightTriangle.hs

--- rightTriangle/rightTriangle.hs
+++ rightTriangle/rightTriangle.hs
@@ -12,9 +12,20 @@ main = do
 	
 	checking :: Int -> Int -> Int -> String
 	let checking a b c =
-		if a^2 + b^2 == c^2 {
-			then putStrLn "it is a right triangle"
-		}
-			else do putStrLn "it is not a right triangle"
+		if (a^2 + b^2 == c^2 )
+			then (putStrLn "it is a right triangle")
+
+			else (putStrLn "it is not a right triangle")
 	
---rightTriangle.hs:16:25: parse error on input `then'
+{-ghc --make "rightTriangle.hs" (in directory: /home/mr-fool/Documents/haskell/rightTriangle)
+[1 of 1] Compiling Main             ( rightTriangle.hs, rightTriangle.o )
+rightTriangle.hs:13:9: Not in scope: `checking'
+rightTriangle.hs:14:9:
+    The last statement in a 'do' block must be an expression
+      let checking a b c
+            = if (a ^ 2 + b ^ 2 == c ^ 2) then
+                  (putStrLn "it is a right triangle")
+              else
+                  (putStrLn "it is not a right triangle")
+Compilation failed.
+-}
