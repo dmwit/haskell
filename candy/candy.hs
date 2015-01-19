5015562a2557e487b89a0ccc457945afb4035a3e
mr-fool <mr-fool@users.noreply.github.com>
2015-01-19 10:07:58 -0700

    Create candy.hs

---
+++ candy/candy.hs
@@ -0,0 +1,10 @@
+module Main where
+main = do
+	putStrLn "What is your favorite type of candy"
+	candy <- getLine
+	putStr candy
+	putStrLn " is a good kid, yeah."
+	putStrLn "You have fine taste, sir!"
+	putStr "Regards, "
+	putStrLn "Your Computer." 
+	
