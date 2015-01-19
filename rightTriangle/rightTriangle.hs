2be909ff70525d43e1c89eeb1b6d3c9a164ac0d6
mr-fool <mr-fool@users.noreply.github.com>
2015-01-18 23:06:36 -0700

    Update rightTriangle.hs

--- rightTriangle/rightTriangle.hs
+++ rightTriangle/rightTriangle.hs
@@ -1,20 +1,25 @@
 main = do {
-
 	--taking sides input
 	
 	; putStrLn "Please enter the length of a"
 	; a <- readLn
+	
 	; putStrLn "Please enter the length of b"
 	; b <- readLn
 	
 	; putStrLn "Please enter the length of c"
 	; c <- readLn
 	
+	;if (null a || null b || null c)
+		then return ()
+		else do checking
+	
 	; let checking :: Int -> Int -> Int -> IO ()
 	      checking a b c =
 		if (a^2 + b^2 == c^2 && c >= 1 && b >= 1 && b <= c && a >=1 && a <=b)
-			then putStrLn "it is a right triangle"
-			else do putStrLn "it is not a right triangle"
+			then putStrLn ("it is a right triangle")
+			else do putStrLn ("it is not a right triangle")
         ; checking a b c
         ; return () 
  }
+--rightTriangle.hs:15:25: Not in scope: `checking'
