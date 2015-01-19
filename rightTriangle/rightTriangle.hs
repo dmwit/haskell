43e1ef85eb9eafe321fb075d90d3430a96e728cb
mr-fool <mr-fool@users.noreply.github.com>
2015-01-18 21:44:12 -0700

    Update rightTriangle.hs

--- rightTriangle/rightTriangle.hs
+++ rightTriangle/rightTriangle.hs
@@ -12,7 +12,7 @@ main = do
 	c <- getLine
 	
 	let rightTriangle = [(a,b,c) |c <- [1..] , b <- [1..c] , a <- [1..b] , a^2 + b^2 == c^2]
-	checking :: (Int, Int, Int) -> String
+	checking :: Int -> Int -> Int -> String
 	let checking a b c =
 	if rightTriangle == True
 		then putStrLn "it is a right triangle"
