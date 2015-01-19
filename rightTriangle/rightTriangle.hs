56f9449b1424fc1dcf0bd0f225adf04d5046c9ca
mr-fool <mr-fool@users.noreply.github.com>
2015-01-18 21:11:36 -0700

    Update rightTriangle.hs

--- rightTriangle/rightTriangle.hs
+++ rightTriangle/rightTriangle.hs
@@ -1,3 +1,1 @@
-let rightTrianges = [ (a,b,c) | c <- [1..], b <- [1..], a <- [1..], a^2 + b^2 = c^2]
-rightTriangle.hs:3:1:
-    parse error (possibly incorrect indentation or mismatched brackets)
+let rightTrianges = [ (a,b,c) | c <- [1..], b <- [1..c], a <- [1..b], a^2 + b^2 = c^2]
