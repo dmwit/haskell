a7ee22dab8d07b3210e0a54c5caff3712611ebd1
mr-fool <mr-fool@users.noreply.github.com>
2015-01-18 22:00:36 -0700

    Update rightTriangle.hs

--- rightTriangle/rightTriangle.hs
+++ rightTriangle/rightTriangle.hs
@@ -4,18 +4,17 @@ main = do
 	
 	putStrLn "Please enter the length of a"
 	a <- getLine
-	
 	putStrLn "Please enter the length of b"
 	b <- getLine
 	
 	putStrLn "Please enter the length of c"
 	c <- getLine
 	
-	let rightTriangle = [(a,b,c) |c <- [1..] , b <- [1..c] , a <- [1..b] , a^2 + b^2 == c^2]
 	checking :: Int -> Int -> Int -> String
 	let checking a b c =
-	if rightTriangle == True
-		then putStrLn "it is a right triangle"
+		if a^2 + b^2 == c^2 {
+			then putStrLn "it is a right triangle"
+		}
+			else do putStrLn "it is not a right triangle"
 	
-	else putStrLn "it is not a right triangle"
-	
+--rightTriangle.hs:16:25: parse error on input `then'
