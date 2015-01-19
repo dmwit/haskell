ca9184f8f3160cafe8b28e35eeb39f98a95066ee
mr-fool <mr-fool@users.noreply.github.com>
2015-01-19 09:45:57 -0700

    Create panzerlied.hs

---
+++ panzerlied/panzerlied.hs
@@ -0,0 +1,32 @@
+module Main where
+main = do
+	putStrLn "\nFirst stanza"
+	
+	putStrLn "Ob\'s stürmt oder schneit, ob die Sonne uns lacht"
+	putStrLn "Der Tag glühend heiß, oder eiskalt die Nacht,"
+	putStrLn "Bestaubt sind die Gesichter, doch froh ist unser Sinn, ja, unser Sinn."
+	putStrLn "Es braust unser Panzer im Sturmwind dahin. (x2)\n"
+	
+	putStrLn "Second stanza"
+	putStrLn "Mit donnerndem Motor, geschwind wie der Blitz,"
+	putStrLn "Dem Feinde entgegen, im Panzer geschützt"
+	putStrLn "Voraus den Kameraden, Im Kampfe ganz allein, Ja ganz allein."
+	putStrLn "So stoßen wir tief in die feindlichen Reih'n! (x2)\n"
+	
+	putStrLn "Third stanza"
+	putStrLn "Wenn vor uns ein feindlicher Panzer erscheint,"
+	putStrLn "Wird Vollgas gegeben und ran an den Feind."
+	putStrLn "Was gilt denn unser Leben für uns'res Reiches Heer? Ja, Reiches Heer."
+	putStrLn "Für Deutschland zu sterben ist unsre höchste Ehr'. (x2)\n"
+	
+	putStrLn "Fourth stanza"
+	putStrLn "Mit Sperren und Minen hält der Gegner uns auf,"
+	putStrLn "Wir lachen darüber und fahren nicht drauf."
+	putStrLn "Und droh'n vor uns Geschütze Versteckt im gelben Sand, Ja, gelben Sand."
+	putStrLn "Wir suchen uns Wege, die keiner sonst fand. (x2)\n"
+	
+	putStrLn "Fifth stanza"
+	putStrLn "Und läßt uns im Stich einst das treulose Glück,"
+	putStrLn "Und kehren wir nicht mehr zur Heimat zurück,"
+	putStrLn "Trifft uns die Todeskugel, ruft uns das Schicksal ab, Ja, Schicksal ab."
+	putStrLn "Dann wird unser Panzer ein ehernes Grab! (x2)"
