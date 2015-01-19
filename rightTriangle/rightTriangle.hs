ba442f974f95734e6c56c5f8b43d30b5e4a70f9c
mr-fool <mr-fool@users.noreply.github.com>
2015-01-18 22:32:17 -0700

    Update rightTriangle.hs

--- rightTriangle/rightTriangle.hs
+++ rightTriangle/rightTriangle.hs
@@ -11,21 +11,10 @@ main = do
 	c <- readLn
 	
 	let checking :: Int -> Int -> Int -> String
-		checking a b c =
+	checking a b c =
 		if (a^2 + b^2 == c^2 )
 			then (putStrLn "it is a right triangle")
 
 			else (putStrLn "it is not a right triangle")
 	
-{-ghc --make "rightTriangle.hs" (in directory: /home/mr-fool/Documents/haskell/rightTriangle)
-[1 of 1] Compiling Main             ( rightTriangle.hs, rightTriangle.o )
-rightTriangle.hs:13:9: Not in scope: `checking'
-rightTriangle.hs:14:9:
-    The last statement in a 'do' block must be an expression
-      let checking a b c
-            = if (a ^ 2 + b ^ 2 == c ^ 2) then
-                  (putStrLn "it is a right triangle")
-              else
-                  (putStrLn "it is not a right triangle")
-Compilation failed.
--}
+--rightTriangle.hs:14:24: parse error on input `='
