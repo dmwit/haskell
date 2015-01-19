74506bc0c17a9c2f20045860ed3560b4c4455e9a
mr-fool <mr-fool@users.noreply.github.com>
2015-01-19 10:18:31 -0700

    Update removeNonUppercase.hs

--- removeNonUppercase/removeNonUppercase.hs
+++ removeNonUppercase/removeNonUppercase.hs
@@ -1,14 +1,8 @@
 main = do
 	--taking user input
 	putStrLn "Please enter a String that you want to remove lowercase"
-	input <- readLn
-	print (removeNonUppercase input)
+	input <- getLine
+	putStrLn (removeNonUppercase input)
 	
 removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
-
--{
-Please enter a String that you want to remove lowercase
-fuck
-removeNonUppercase.hs: user error (Prelude.readIO: no parse)
--}
