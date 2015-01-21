6eb350783db85a2e7b13276b9a83bc6692db6252
mr-fool <mr-fool@users.noreply.github.com>
2015-01-21 14:36:10 -0700

    Update reverse.hs

--- reverse/reverse.hs
+++ reverse/reverse.hs
@@ -1,13 +1,23 @@
 import Data.List
+import Text.Read
 myLast = reverse
 main = do 
 	putStrLn "Please enter a list"
-	a <- getLine
+	a <- read
+	putStrLn "Reversing your list"
 	putStrLn (myLast a)
 
--{
-runhaskell myLast.hs
-Please enter a list
-[1,2,3]
-]3,2,1[
--}
+-[
+[1 of 1] Compiling Main             ( myLast.hs, myLast.o )
+myLast.hs:6:14:
+    Couldn't match expected type ‘IO [Char]’
+                with actual type ‘String -> a0’
+    Probable cause: ‘read’ is applied to too few arguments
+    In a stmt of a 'do' block: a <- read
+    In the expression:
+      do { putStrLn "Please enter a list";
+           a <- read;
+           putStrLn "Reversing your list";
+           putStrLn (myLast a) }
+Compilation failed.
+-]
