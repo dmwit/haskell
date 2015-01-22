afd8519e9123deb64b74f416c18408bf6acdebac
mr-fool <mr-fool@users.noreply.github.com>
2015-01-21 20:00:23 -0700

    Update female.hs

--- female/female.hs
+++ female/female.hs
@@ -1,14 +1,15 @@
 module Main where
 import Data.Char
 
-uppercase :: String -> String
-uppercase = map toUpper
 
 main = do
 	putStrLn "Guess a female name that I'm thinking of ..."
 	guess <- getLine
 	
 	--changing the input to uppercase for comparision
+	let uppercase :: String -> String
+	uppercase = map toUpper
+	
 	let uppercaseString = uppercase guess
 	case uppercaseString of 
 		"CORTANA"	-> 	putStrLn "Nerd"
