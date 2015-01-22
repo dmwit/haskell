89f981e47106374afa2b968db8fd8e2599282c5a
mr-fool <mr-fool@users.noreply.github.com>
2015-01-21 21:51:26 -0700

    Update login.hs

--- login/login.hs
+++ login/login.hs
@@ -1,10 +1,9 @@
 import Data.Char
 
 main = do
-	putStrLn "Please remember username and password are case sensitve"
+    putStrLn "Please remember username and password are case sensitve"
     putStrLn "Username:"
     username <- getLine
     putStrLn "Password:"
     password <-getLine
     putStrLn "Authemticating..."
---login.hs:6:5: parse error on input ‘putStrLn’
