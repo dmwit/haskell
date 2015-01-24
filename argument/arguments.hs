058c4d7ef418769a566f000c2d8324ba7e21b226
mr-fool <mr-fool@users.noreply.github.com>
2015-01-24 14:05:19 -0700

    Update arguments.hs

--- argument/arguments.hs
+++ argument/arguments.hs
@@ -1,26 +1,16 @@
 import System.Environment
 
 main = do
-    [args1,args2] <- getArgs 
-    case args1 of
-        ""	-> putStrLn "There is no input"
-        _	-> case args2 of 
-		    ""	-> putStrLn "Missing second argument"
-		    _	-> putStrLn "There are two inputs"
-        
--{ 
-runhaskell argument.hs 1
-argument.hs: user error (Pattern match failure in do expression at argument.hs:4:5-17)
-
-[mr-fool@localhost argument]$ runhaskell argument.hs 1 2
-There are two inputs
-
-[mr-fool@localhost argument]$ runhaskell argument.hs 1
-
-argument.hs: user error (Pattern match failure in do expression at argument.hs:4:5-17)
-[mr-fool@localhost argument]$ runhaskell argument.hs 
-argument.hs: user error (Pattern match failure in do expression at argument.hs:4:5-17)
-
+    args	<-	getArgs
+    putStrLn ("You gave " ++ length args ++ " arguments")
+    
+-{
+ghc --make "argument.hs" (in directory: /home/mr-fool/Documents/haskell/argument)
+[1 of 1] Compiling Main             ( argument.hs, argument.o )
+argument.hs:5:30:
+    Couldn't match expected type ‘[Char]’ with actual type ‘Int’
+    In the first argument of ‘(++)’, namely ‘length args’
+    In the second argument of ‘(++)’, namely
+      ‘length args ++ " arguments"’
+Compilation failed.
 -}
-
-
