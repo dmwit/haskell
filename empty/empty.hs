4b06853d8cbd4f138fbfd27b6b3f587a86392dbb
mr-fool <mr-fool@users.noreply.github.com>
2015-01-24 12:41:54 -0700

    Update empty.hs

--- empty/empty.hs
+++ empty/empty.hs
@@ -6,19 +6,10 @@ main = do
     empty2 <- getLine
     
     putStrLn "Just click enter"
-    empty2 <- getLine
+    empty3 <- getLine
     
     let newEmpty = empty1 ++ empty2 ++ empty3
-    putStrLn "last"
--{
-ghc --make "empty.hs" (in directory: /home/mr-fool/Documents/haskell/empty)
-[1 of 1] Compiling Main             ( empty.hs, empty.o )
-empty.hs:11:40:
-    Not in scope: ‘empty3’
-    Perhaps you meant one of these:
-      ‘empty1’ (line 3), ‘empty2’ (line 9)
-Compilation failed.
- -}
+    
 
     
     
