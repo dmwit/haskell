f83a690624cc0a964a3d6c81ca038d53946f0e74
mr-fool <mr-fool@users.noreply.github.com>
2015-01-24 21:42:50 -0700

    Update calculator.hs

--- calculator/calculator.hs
+++ calculator/calculator.hs
@@ -8,9 +8,20 @@ main = do
     putStrLn "Enter the second number."
     num2	<-	getLine
 
-    case operator of operation ->
-        let operation = "a" in (case operation of "a" -> (+); "s" -> (-); "m"-> (*))
+    case operation of
+        let operation = "a" in 
+            case operation of "a" -> (+)
+                "s" -> (-)
+                "m"-> (*))
         
     let answer = show(num1) operation show(num2)
     putStrLn("hello")
     
+-{
+ghc --make "calculator.hs" (in directory: /home/mr-fool/Documents/haskell/calculator)
+[1 of 1] Compiling Main             ( calculator.hs, calculator.o )
+calculator.hs:12:9:
+    Parse error in pattern: let operation = "a"
+                            in case operation of { "a" -> (+) }
+Compilation failed.
+}-
